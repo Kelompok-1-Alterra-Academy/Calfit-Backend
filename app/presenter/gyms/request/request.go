e0b96f2ce674172d4ff9351f223bcf9be2deb23f
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-12-22 15:32:56 +0700

    feat: add request & response for gyms endpoints

--- app/presenter/gyms/request/request.go
+++ app/presenter/gyms/request/request.go
@@ -1,1 +1,13 @@
 package request
+
+type KeyStruct struct {
+	Key string `json:"key"`
+}
+
+type CreateGym struct {
+	Name      string `json:"name" form:"name"`
+	Telephone string `json:"telephone" form:"telephone"`
+	// Picture        string   `json:"picture" form:"picture"`
+	Operational_admin_ID uint `json:"operational_admin_id" form:"operational_admin_id"`
+	Address_ID           uint `json:"address_id" form:"address_id"`
+}
--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,19 +1,31 @@
 package routes
 
 import (
+	"CalFit/controllers/gyms"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type HandlerList struct {
 	JWTMiddleware middleware.JWTConfig
+	GymController *gyms.GymController
 }
 
-func (handler *HandlerList) RouteRegister(e *echo.Echo) {
+func (handler HandlerList) RouteRegister(e *echo.Echo) {
 	e.Pre(middleware.RemoveTrailingSlash())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+
+	v1 := e.Group("/api/v1")
+	v1.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
+	v1.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${method} ${uri} ${status} ${time_rfc3339} ${latency_human}\n",
+	}))
 
+	// unprotected routes
+	{
+		v1.GET("/gyms", handler.GymController.GetAll)
+	}
 }
