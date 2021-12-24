e3157d3ab59da499df62cde0575260abf940d28b
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-12-24 10:59:06 +0700

    fix: rebase conflict

--- business/gyms/usecase.go
+++ business/gyms/usecase.go
@@ -1,1 +1,72 @@
 package gyms
+
+import (
+	context "context"
+	"time"
+	// "github.com/go-playground/validator/v10"
+)
+
+type Usecase struct {
+	Repo           DomainRepository
+	ContextTimeout time.Duration
+}
+
+func NewUsecase(repo DomainRepository, timeout time.Duration) *Usecase {
+	return &Usecase{
+		Repo:           repo,
+		ContextTimeout: timeout,
+	}
+}
+
+func (u *Usecase) GetAll(ctx context.Context) ([]Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, u.ContextTimeout)
+	defer cancel()
+
+	return u.Repo.GetAll(ctx)
+}
+
+// func (u *Usecase) GetById(ctx context.Context, id string) (Domain, error) {
+// 	ctx, cancel := context.WithTimeout(ctx, u.ContextTimeout)
+// 	defer cancel()
+
+// 	if id == "" {
+// 		return Domain{}, exceptions.ErrEmptyInput
+// 	}
+
+// 	return u.Repo.GetById(ctx, id)
+// }
+
+// func (u *Usecase) GetByISBN(ctx context.Context, isbn string) (Domain, error) {
+// 	ctx, cancel := context.WithTimeout(ctx, u.ContextTimeout)
+// 	defer cancel()
+
+// 	if isbn == "" {
+// 		return Domain{}, exceptions.ErrEmptyInput
+// 	}
+
+// 	return u.Repo.GetByISBN(ctx, isbn)
+// }
+
+// func (u *Usecase) Create(ctx context.Context, domain Domain) (Domain, error) {
+// 	ctx, cancel := context.WithTimeout(ctx, u.ContextTimeout)
+// 	defer cancel()
+
+// 	validate := validator.New()
+// 	err := validate.Struct(domain)
+// 	if err != nil {
+// 		return Domain{}, exceptions.ErrValidationFailed
+// 	}
+
+// 	return u.Repo.Create(ctx, domain)
+// }
+
+// func (u *Usecase) UpdateStatus(ctx context.Context, bookId string, status bool) (Domain, error) {
+// 	ctx, cancel := context.WithTimeout(ctx, u.ContextTimeout)
+// 	defer cancel()
+
+// 	if bookId == "" {
+// 		return Domain{}, exceptions.ErrEmptyInput
+// 	}
+
+// 	return u.Repo.UpdateStatus(ctx, bookId, status)
+// }
--- repository/mysql/gyms/record.go
+++ repository/mysql/gyms/record.go
@@ -4,18 +4,65 @@ import (
 	"CalFit/repository/mysql/addresses"
 	"CalFit/repository/mysql/classes"
 	"CalFit/repository/mysql/operational_admins"
+	"time"
 )
 
 type Gym struct {
-	Id                  int `gorm:"primaryKey"`
-	Name                string
-	Telephone           string
-	Picture             string
-	Operational_adminID int
-	AddressID           int
+	Id                  uint `gorm:"primaryKey"`
+	Name                string `gorm:"type:varchar(100);not null"`
+	Telephone           string `gorm:"type:varchar(20);not null"`
+	Picture             string `gorm:"type:varchar(500);not null"`
+	Operational_adminID uint `gorm:"not null"`
+	AddressID           uint `gorm:"not null"`
 	Operational_admin   operational_admins.Operational_admin
-	Address             addresses.Address
+	Address             addresses.Address 
 	Classes             []classes.Class
-	Created_at          string
-	Updated_at          string
+	Created_at          time.Time
+	Updated_at          time.Time
 }
+
+func (g *Gym) BeforeCreate() error {
+	g.Created_at = time.Now()
+	g.Updated_at = time.Now()
+	return nil
+}
+
+// func (g *Gym) ToDomain() gyms.Domain {
+// 	return gyms.Domain{
+// 		Id:                  g.Id,
+// 		Name:                g.Name,
+// 		Telephone:           g.Telephone,
+// 		Picture:             g.Picture,
+// 		Operational_adminID: g.Operational_adminID,
+// 		AddressID:           g.AddressID,
+// 		Operational_admin:   g.Operational_admin.ToDomain(),
+// 		Address:             g.Address.ToDomain(),
+// 		Classes:             g.Classes.ToDomain(),
+// 		Created_at:          g.Created_at,
+// 		Updated_at:          g.Updated_at,
+// 	}
+// }
+
+// func FromDomain(domain gyms.Domain) Gym {
+// 	return Gym{
+// 		Id:                  domain.Id,
+// 		Name:                domain.Name,
+// 		Telephone:           domain.Telephone,
+// 		Picture:             domain.Picture,
+// 		Operational_adminID: domain.Operational_adminID,
+// 		AddressID:           domain.AddressID,
+// 		Operational_admin:   domain.Operational_admin.FromDomain(),
+// 		Address:             domain.Address.FromDomain(),
+// 		Classes:             domain.Classes.FromDomain(),
+// 		Created_at:          domain.Created_at,
+// 		Updated_at:          domain.Updated_at,
+// 	}
+// }
+
+// func ToListDomain(list []Gym) []gyms.Domain {
+// 	var result []gyms.Domain
+// 	for _, item := range list {
+// 		result = append(result, item.ToDomain())
+// 	}
+// 	return result
+// }
