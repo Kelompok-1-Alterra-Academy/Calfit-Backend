95d9edb914afdfad69609d1c27e4b52917c91f2e
MuhammadFahmii <fahmiprasetio754@gmail.com>
2021-12-28 19:10:46 +0700

    feat: initialize domain sessions

---
+++ business/sessions/domain.go
@@ -0,0 +1,30 @@
+package sessions
+
+import (
+	"context"
+	"time"
+)
+
+type Domain struct {
+	Id          int
+	Name        string
+	Description string
+	Created_at  time.Time
+	Updated_at  time.Time
+}
+
+type Usecase interface {
+	Insert(ctx context.Context, sessions Domain) (Domain, error)
+	GetAll(ctx context.Context, sessions Domain) ([]Domain, error)
+	GetById(ctx context.Context, sessions Domain) (Domain, error)
+	Update(ctx context.Context, sessions Domain) (Domain, error)
+	Delete(ctx context.Context, sessions Domain) (Domain, error)
+}
+
+type Repository interface {
+	Insert(ctx context.Context, sessions Domain) (Domain, error)
+	GetAll(ctx context.Context, sessions Domain) ([]Domain, error)
+	GetById(ctx context.Context, sessions Domain) (Domain, error)
+	Update(ctx context.Context, sessions Domain) (Domain, error)
+	Delete(ctx context.Context, sessions Domain) (Domain, error)
+}
