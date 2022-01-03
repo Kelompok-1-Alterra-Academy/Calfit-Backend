3121e86efd3ab57c49ea5b04a43fb174b4e1a3c9
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-03 11:59:38 +0700

    feat(users-usecase): adding login usecase

--- business/users/usecase.go
+++ business/users/usecase.go
@@ -20,5 +20,9 @@ func NewUsersUsecase(repo Repository, timeout time.Duration) Usecase {
 func (uu *UsersUsecase) Login(ctx context.Context, users Domain) (Domain, error) {
 	ctx, cancel := context.WithTimeout(ctx, uu.ContextTimeout)
 	defer cancel()
-	return Domain{}, nil
+	res, err := uu.Repo.Login(ctx, users)
+	if err != nil {
+		return Domain{}, err
+	}
+	return res, nil
 }
