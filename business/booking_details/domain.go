69c2e5200e31b7c6d889736f65dd721561310ee8
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-03 10:54:37 +0700

    refactor(bookingdetails-domain): adding domain struct

--- business/booking_details/domain.go
+++ business/booking_details/domain.go
@@ -1,1 +1,15 @@
 package bookingdetails
+
+import "time"
+
+type Domain struct {
+	Id                 int
+	Amount             int
+	Status             string
+	UserID             int
+	OperationalAdminID int
+	PaymentID          int
+	ClassID            int
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+}
