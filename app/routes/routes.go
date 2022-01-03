b53563218930b4f03c918a2b750b5dc02c43245d
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-03 12:43:59 +0700

    refactor(route): adding auth endpoint

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -55,7 +55,11 @@ func (controllers ControllersList) RouteRegister(e *echo.Echo) {
 		v1.GET("/sessions/:id", controllers.SessionsController.GetById)
 
 		// auth endpoint
+<<<<<<< HEAD
 		v1.POST("/auth/login", controllers.AuthController.Login)
+=======
+		v1.GET("/auth/login", controllers.AuthController.Login)
+>>>>>>> refactor(route): adding auth endpoint
 	}
 
 	// superadmin routes
