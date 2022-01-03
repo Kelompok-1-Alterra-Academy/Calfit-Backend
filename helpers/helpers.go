20d6cb14ffa63e0c685a46674afca7b5a5cea1b4
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-03 13:13:05 +0700

    feat(helpers): adding createcookie function

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,1 +1,14 @@
 package helpers
+
+import (
+	"net/http"
+	"time"
+)
+
+func CreateCookie(token string) *http.Cookie {
+	cookie := new(http.Cookie)
+	cookie.Name = "jwt"
+	cookie.Value = "jon"
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	return cookie
+}
