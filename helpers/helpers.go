25ab19a7044ca95ea23599150561641e106815a0
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-03 13:21:09 +0700

    refactor(helpers): adding httponly

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -10,5 +10,6 @@ func CreateCookie(token string) *http.Cookie {
 	cookie.Name = "jwt"
 	cookie.Value = "jon"
 	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.HttpOnly = true
 	return cookie
 }
