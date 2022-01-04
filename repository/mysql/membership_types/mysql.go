6cbe9afafff40d1ed0811fa7da848283394d9146
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-04 12:54:57 +0700

    initiate repository file

---
+++ repository/mysql/membership_types/mysql.go
@@ -0,0 +1,61 @@
+package membership_types
+
+import (
+	"CalFit/business/memberships"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type MembershipsRepo struct {
+	DBConn *gorm.DB
+}
+
+func NewMembershipsRepo(db *gorm.DB) memberships.Repository {
+	return &MembershipsRepo{
+		DBConn: db,
+	}
+}
+
+func (repo *MembershipsRepo) Insert(domain memberships.Domain) (memberships.Domain, error) {
+	data := FromDomain(domain)
+	if err := repo.DBConn.Debug().Create(&data).Error; err != nil {
+		return memberships.Domain{}, err
+	}
+	return data.toDomain(), nil
+}
+
+func (repo *MembershipsRepo) Get(domain memberships.Domain) ([]memberships.Domain, error) {
+	data := []Membership_type{}
+	if err := repo.DBConn.Debug().Find(&data).Error; err != nil {
+		return []memberships.Domain{}, err
+	}
+	var domainMemberships []memberships.Domain
+	for _, val := range data {
+		domainMemberships = append(domainMemberships, val.toDomain())
+	}
+	return domainMemberships, nil
+}
+
+func (repo *MembershipsRepo) Update(domain memberships.Domain) (memberships.Domain, error) {
+	data := FromDomain(domain)
+	repo.DBConn.Debug().Where("id=?", domain.Id)
+	if err := repo.DBConn.Debug().Where("id=?", data.Id).First(&data).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return memberships.Domain{}, errors.New("record not found")
+		}
+		return memberships.Domain{}, err
+	}
+	return data.toDomain(), nil
+}
+
+func (repo *MembershipsRepo) Delete(domain memberships.Domain) (memberships.Domain, error) {
+	data := FromDomain(domain)
+	if err := repo.DBConn.Debug().Where("id=?", data.Id).First(&data).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return memberships.Domain{}, errors.New("record not found")
+		}
+	}
+	repo.DBConn.Where("id=?", data.Id).Delete(&data)
+	return data.toDomain(), nil
+}
--- repository/mysql/membership_types/record.go
+++ repository/mysql/membership_types/record.go
@@ -1,6 +1,7 @@
 package membership_types
 
 import (
+	"CalFit/business/memberships"
 	"CalFit/repository/mysql/classes"
 	"CalFit/repository/mysql/users"
 	"time"
@@ -10,8 +11,27 @@ type Membership_type struct {
 	Id          int `gorm:"primaryKey"`
 	Name        string
 	Description string
-	Users       []users.User
-	Classes     []classes.Class
+	Users       []users.User    `gorm:"foreignkey:UserID"`
+	Classes     []classes.Class `gorm:"foreignkey:GymID"`
 	Created_at  time.Time
 	Updated_at  time.Time
+}
+
+func FromDomain(domain memberships.Domain) Membership_type {
+	return Membership_type{
+		Id:          domain.Id,
+		Name:        domain.Name,
+		Description: domain.Description,
+		Created_at:  time.Now(),
+		Updated_at:  time.Now(),
+	}
+}
+func (s Membership_type) toDomain() memberships.Domain {
+	return memberships.Domain{
+		Id:          s.Id,
+		Name:        s.Name,
+		Description: s.Description,
+		Created_at:  s.Created_at,
+		Updated_at:  s.Updated_at,
+	}
 }
