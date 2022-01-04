909f838edc3967eccf2730aa5a99baf45fffc1b7
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-04 12:36:21 +0700

    initiate business layer

--- business/memberships/domain.go
+++ business/memberships/domain.go
@@ -1,1 +1,27 @@
 package memberships
+
+import (
+	"time"
+)
+
+type Domain struct {
+	Id          int
+	Name        string
+	Description string
+	Created_at  time.Time
+	Updated_at  time.Time
+}
+
+type Usecase interface {
+	Insert(memberships Domain) (Domain, error)
+	Get(memberships Domain) ([]Domain, error)
+	Update(memberships Domain) (Domain, error)
+	Delete(memberships Domain) (Domain, error)
+}
+
+type Repository interface {
+	Insert(memberships Domain) (Domain, error)
+	Get(memberships Domain) ([]Domain, error)
+	Update(memberships Domain) (Domain, error)
+	Delete(memberships Domain) (Domain, error)
+}
--- business/memberships/usecase.go
+++ business/memberships/usecase.go
@@ -1,1 +1,43 @@
 package memberships
+
+type MembershipsUsecase struct {
+	membershipsRepo Repository
+}
+
+func NewMembershipsUsecase(repo Repository) Usecase {
+	return &MembershipsUsecase{
+		membershipsRepo: repo,
+	}
+}
+
+func (membershipsUseCase *MembershipsUsecase) Insert(memberships Domain) (Domain, error) {
+	res, err := membershipsUseCase.membershipsRepo.Insert(memberships)
+	if err != nil {
+		return Domain{}, err
+	}
+	return res, nil
+}
+
+func (membershipsUseCase *MembershipsUsecase) Get(memberships Domain) ([]Domain, error) {
+	res, err := membershipsUseCase.membershipsRepo.Get(memberships)
+	if err != nil {
+		return []Domain{}, err
+	}
+	return res, nil
+}
+
+func (membershipsUseCase *MembershipsUsecase) Update(memberships Domain) (Domain, error) {
+	res, err := membershipsUseCase.membershipsRepo.Update(memberships)
+	if err != nil {
+		return Domain{}, err
+	}
+	return res, nil
+}
+
+func (membershipsUseCase *MembershipsUsecase) Delete(memberships Domain) (Domain, error) {
+	res, err := membershipsUseCase.membershipsRepo.Delete(memberships)
+	if err != nil {
+		return Domain{}, err
+	}
+	return res, nil
+}
