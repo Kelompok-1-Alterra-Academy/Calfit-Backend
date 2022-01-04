079d33abdcfee82842acc42174d0c107759bd353
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-04 18:09:28 +0700

    refactor(schedules-usecase): adding defer when timeout is over

--- business/schedules/usecase.go
+++ business/schedules/usecase.go
@@ -18,6 +18,8 @@ func NewSchedulesUsecase(repo Repository, timeout time.Duration) Usecase {
 }
 
 func (schedulesUseCase *SchedulesUsecase) Insert(ctx context.Context, schedules Domain) (Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, schedulesUseCase.ContextTimeout)
+	defer cancel()
 	res, err := schedulesUseCase.Repo.Insert(ctx, schedules)
 	if err != nil {
 		return Domain{}, err
@@ -26,6 +28,8 @@ func (schedulesUseCase *SchedulesUsecase) Insert(ctx context.Context, schedules 
 }
 
 func (schedulesUseCase *SchedulesUsecase) Get(ctx context.Context) ([]Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, schedulesUseCase.ContextTimeout)
+	defer cancel()
 	res, err := schedulesUseCase.Repo.Get(ctx)
 	if err != nil {
 		return []Domain{}, err
@@ -34,6 +38,8 @@ func (schedulesUseCase *SchedulesUsecase) Get(ctx context.Context) ([]Domain, er
 }
 
 func (schedulesUseCase *SchedulesUsecase) Update(ctx context.Context, schedules Domain) (Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, schedulesUseCase.ContextTimeout)
+	defer cancel()
 	res, err := schedulesUseCase.Repo.Update(ctx, schedules)
 	if err != nil {
 		return Domain{}, err
@@ -42,6 +48,8 @@ func (schedulesUseCase *SchedulesUsecase) Update(ctx context.Context, schedules 
 }
 
 func (schedulesUseCase *SchedulesUsecase) Delete(ctx context.Context, id int) (Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, schedulesUseCase.ContextTimeout)
+	defer cancel()
 	res, err := schedulesUseCase.Repo.Delete(ctx, id)
 	if err != nil {
 		return Domain{}, err
