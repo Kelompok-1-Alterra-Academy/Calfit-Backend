7abf1a8e4da6c2a43c387351a0dbe90f3eeeb47e
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-04 13:38:54 +0700

    initiate controllers file

--- controllers/memberships/request/request.go
+++ controllers/memberships/request/request.go
@@ -1,1 +1,32 @@
 package request
+
+import (
+	"CalFit/business/memberships"
+	"time"
+)
+
+type Memberships struct {
+	Id          int    `json:"id" form:"id"`
+	Name        string `json:"name" form:"name"`
+	Description string `json:"description" form:"description"`
+}
+
+func ToDomain(s Memberships) memberships.Domain {
+	if s.Id == 0 {
+		return memberships.Domain{
+			Name:        s.Name,
+			Description: s.Description,
+			Created_at:  time.Now(),
+		}
+	} else if s.Id != 0 {
+		return memberships.Domain{
+			Id:          s.Id,
+			Name:        s.Name,
+			Description: s.Description,
+			Updated_at:  time.Now(),
+		}
+	}
+	return memberships.Domain{
+		Id: s.Id,
+	}
+}
--- controllers/memberships/response/response.go
+++ controllers/memberships/response/response.go
@@ -1,1 +1,24 @@
-package request
+package response
+
+import (
+	"CalFit/business/memberships"
+	"time"
+)
+
+type Memberships struct {
+	Id          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Created_at  time.Time `json:"created_at"`
+	Updated_at  time.Time `json:"updated_at"`
+}
+
+func FromDomain(s memberships.Domain) Memberships {
+	return Memberships{
+		Id:          s.Id,
+		Name:        s.Name,
+		Description: s.Description,
+		Created_at:  s.Created_at,
+		Updated_at:  s.Updated_at,
+	}
+}
