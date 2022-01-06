819749f00b16d3d41c97b5b4e2d125b236301b6b
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-06 10:16:29 +0700

    refactor(users-usecase): adding register usecase

--- business/users/usecase.go
+++ business/users/usecase.go
@@ -34,3 +34,16 @@ func (uu *UsersUsecase) Login(ctx context.Context, users Domain) (Domain, error)
 	res.Token, _ = uu.JWTAuth.GenerateToken(res.Email)
 	return res, nil
 }
+
+func (uu *UsersUsecase) Register(ctx context.Context, users Domain) (Domain, error) {
+	ctx, cancel := context.WithTimeout(ctx, uu.ContextTimeout)
+	defer cancel()
+	if users.Email == "" || users.Password == "" {
+		return Domain{}, exceptions.ErrInvalidCredentials
+	}
+	res, err := uu.Repo.Register(ctx, users)
+	if err != nil {
+		return Domain{}, err
+	}
+	return res, nil
+}
