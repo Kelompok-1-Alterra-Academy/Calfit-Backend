19cca3f37b3afd4a64576bd111dece4113a4ec3c
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-06 10:18:36 +0700

    refactor(users-repo): adding register repository

--- repository/mysql/users/mysql.go
+++ repository/mysql/users/mysql.go
@@ -46,3 +46,27 @@ func (repo *UsersRepo) Login(ctx context.Context, domain users.Domain) (users.Do
 	}
 	return data.ToDomain(), nil
 }
+
+func (repo *UsersRepo) Register(ctx context.Context, domain users.Domain) (users.Domain, error) {
+	data := FromDomain(domain)
+	if err := repo.DBConn.Debug().Where("email=?", data.Email).First(&data).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			data.MembershipTypeID = 1
+			data.CreatedAt = time.Now()
+			address := addresses.Address{
+				Address:     "default",
+				District:    "default",
+				City:        "default",
+				Postal_code: "default",
+			}
+			repo.DBConn.Debug().Create(&address)
+			data.AddressID = address.Id
+			repo.DBConn.Debug().Create(&data)
+			return data.ToDomain(), nil
+		} else if errors.As(err, &mysqlErr) && mysqlErr.Number == 1062 {
+			return data.ToDomain(), nil
+		}
+		return users.Domain{}, err
+	}
+	return data.ToDomain(), nil
+}
