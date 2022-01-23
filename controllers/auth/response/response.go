ecf75fcc5fa5cc4a5d79b18b4af48efd55d87605
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-23 12:31:03 +0700

    refactor(auth-res): adding more information on struct

--- controllers/auth/response/response.go
+++ controllers/auth/response/response.go
@@ -1,12 +1,18 @@
 package response
 
-import "CalFit/business/users"
+import (
+	"CalFit/business/users"
+	"time"
+)
 
 type Auth struct {
-	Email          string `json:"email"`
-	Username       string `json:"username"`
-	Token          string `json:"token,omitempty"`
-	MembershipName string `json:"membership_name,omitempty"`
+	Email          string     `json:"email"`
+	Username       string     `json:"username"`
+	Token          string     `json:"token,omitempty"`
+	MembershipName string     `json:"membership_name,omitempty"`
+	Photo          string     `json:"photo,omitempty"`
+	CreatedAt      *time.Time `json:"created_at,omitempty"`
+	UpdatedAt      *time.Time `json:"updated_at,omitempty"`
 }
 
 func FromDomain(u users.Domain) Auth {
@@ -15,5 +21,8 @@ func FromDomain(u users.Domain) Auth {
 		Username:       u.Username,
 		Token:          u.Token,
 		MembershipName: u.MembershipName,
+		Photo:          u.Photo,
+		CreatedAt:      &u.CreatedAt,
+		UpdatedAt:      &u.UpdatedAt,
 	}
 }
