5cf2eb4bba8e137abd659a807b2bb086eea8cd4c
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-23 10:18:22 +0700

    refactor(users-res): adding omitempty jsontag

--- controllers/auth/response/response.go
+++ controllers/auth/response/response.go
@@ -6,7 +6,7 @@ type Auth struct {
 	Email          string `json:"email"`
 	Username       string `json:"username"`
 	Token          string `json:"token,omitempty"`
-	MembershipName string `json:"membership_name"`
+	MembershipName string `json:"membership_name,omitempty"`
 }
 
 func FromDomain(u users.Domain) Auth {
