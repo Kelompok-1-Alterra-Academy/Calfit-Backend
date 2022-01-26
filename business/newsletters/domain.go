dca52f1c9418602ce356b3b9757992177dc95ad1
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-26 11:39:04 +0700

    featuring in business and repository layer

--- business/newsletters/domain.go
+++ business/newsletters/domain.go
@@ -1,1 +1,35 @@
 package newsletters
+
+import (
+	"CalFit/business/paginations"
+	context "context"
+	"time"
+)
+
+type Domain struct {
+	Id                  int
+	Title               string `validate:"required"`
+	Description         string `validate:"required"`
+	Content             string `validate:"required"`
+	Operational_adminID int    `validate:"required"`
+	Created_at          time.Time
+	Updated_at          time.Time
+}
+
+type Usecase interface {
+	GetAll(ctx context.Context, pagination paginations.Domain) ([]Domain, error)
+	CountAll(ctx context.Context) (int, error)
+	GetById(ctx context.Context, id string) (Domain, error)
+	Create(ctx context.Context, domain Domain, Id string) (Domain, error)
+	Update(ctx context.Context, id string, domain Domain) (Domain, error)
+	Delete(ctx context.Context, id string) error
+}
+
+type Repository interface {
+	GetAll(ctx context.Context, pagination paginations.Domain) ([]Domain, error)
+	CountAll(ctx context.Context) (int, error)
+	GetById(ctx context.Context, id string) (Domain, error)
+	Create(ctx context.Context, domain Domain) (Domain, error)
+	Update(ctx context.Context, id string, domain Domain) (Domain, error)
+	Delete(ctx context.Context, id string) error
+}
--- repository/mysql/newsletters/record.go
+++ repository/mysql/newsletters/record.go
@@ -1,12 +1,56 @@
 package newsletters
 
-import "time"
+import (
+	"CalFit/business/newsletters"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Newsletter struct {
 	Id                  int `gorm:"primaryKey"`
 	Title               string
+	Description         string
 	Content             string
 	Operational_adminID int
 	Created_at          time.Time
 	Updated_at          time.Time
+}
+
+func (n *Newsletter) BeforeCreate(tx *gorm.DB) error {
+	n.Created_at = time.Now()
+	n.Updated_at = time.Now()
+	return nil
+}
+
+func (n *Newsletter) ToDomain() newsletters.Domain {
+	return newsletters.Domain{
+		Id:                  n.Id,
+		Title:               n.Title,
+		Description:         n.Description,
+		Content:             n.Content,
+		Operational_adminID: n.Operational_adminID,
+		Created_at:          n.Created_at,
+		Updated_at:          n.Updated_at,
+	}
+}
+
+func FromDomain(domain newsletters.Domain) Newsletter {
+	return Newsletter{
+		Id:                  domain.Id,
+		Title:               domain.Title,
+		Description:         domain.Description,
+		Content:             domain.Content,
+		Operational_adminID: domain.Operational_adminID,
+		Created_at:          domain.Created_at,
+		Updated_at:          domain.Updated_at,
+	}
+}
+
+func ToListDomain(data []Newsletter) []newsletters.Domain {
+	var listDomain []newsletters.Domain
+	for _, item := range data {
+		listDomain = append(listDomain, item.ToDomain())
+	}
+	return listDomain
 }
